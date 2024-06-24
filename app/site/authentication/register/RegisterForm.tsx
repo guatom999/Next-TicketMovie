726d59199713e72bc416e7d7394eaf9cb29a47fc
guatom999 <35306045+guatom999@users.noreply.github.com>
2024-06-25 03:53:44 +0700

    Fix Import Modal

--- app/site/authentication/register/RegisterForm.tsx
+++ app/site/authentication/register/RegisterForm.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch, SetStateAction } from 'react'
-import Modal from "../../components/Modal"
+import Modal from "../../../components/Modal"
 
 type Props = {
     isLoading: boolean
