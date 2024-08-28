cd5de95b37589547ef4304cf0d78310b3c3c511d
guatom999 <35306045+guatom999@users.noreply.github.com>
2024-08-28 23:56:57 +0700

    add movie detail

--- app/movie/[movieid]/Movieshowdate.tsx
+++ app/movie/[movieid]/Movieshowdate.tsx
@@ -25,22 +25,30 @@ const Movieshowdate = ({ movid_id, movieDetailIndex, movieList, movieDetail, mov
     const [showDate, setShowDate] = useState("")
     const [reserveSeat, setReserveSeat] = useState<string[]>([])
     const [isSelected, setIsSelected] = useState<Boolean[]>(Array(48).fill(false))
+    const [localtime, setLocaltime] = useState(GetLocalTime())
     const { data: session } = useSession()
 
     //Need Refactor
     const seat = ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "C1",
         "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "D12"]
 
-
     const movieDetailSeparate = movieDetail
-
-    console.log("GetLocalTime is :::::::::::::::::> ",GetLocalTime())
 
     useEffect(() => {
         // setIsSelected([false, false, false, false, false, false, false, false, false, false, false, false])
         setReserveSeat([])
 
     }, [movieIndex])
+
+    const comparingTime = () => {
+        console.log("localtimne parse", Date.parse(GetLocalTime()), "movie relase time", Date.parse(movieDate[0]))
+        if (Date.parse(GetLocalTime()) > Date.parse(movieDate[0])) {
+            return true
+        }
+        return false
+    }
+
+
 
     const isThatButton = (data: string) => {
         if (data == showTime) {
@@ -81,117 +89,137 @@ const Movieshowdate = ({ movid_id, movieDetailIndex, movieList, movieDetail, mov
                         height={100}
                     />
                 </div>
-                <div className="flex justify-center p-10">
-                    {/* <p>${movieList?.data}</p> */}
+                <div className="flex flex-col items-center p-5 gap-4">
+                    <p className="text-bold font-sans text-2xl">{movieList?.title}</p>
+                    <p>{movieList?.running_time} </p>
+                    <p className="text-center ">{movieList?.description}</p>
                 </div>
             </div>
 
-            {/* {
-                movieDate[0] < GetLocalTime ? (<></>) : (<></>)
-            } */}
-            <div className="w-full p-4">
-                <div className="font-serif font-semibold text-2xl">SHOWTIMES</div>
-                <div className="py-10">
-                    {movieDate.map((date: any, i: number) => (
-                        <div
-                            key={i}
-                            className="">
-                            <p>{date}</p>
-                            <div className=" flex flex-row gap-2">
-                                {movieDetailShowCase[i].map((data: any, index: number) => (
+            <>
+                {
+                    comparingTime() ? (<>
+                        <div className="w-full p-4">
+                            <div className="font-serif font-semibold text-2xl">SHOWTIMES</div>
+                            <div className="py-10">
+                                {movieDate.map((date: any, i: number) => (
                                     <div
-                                        // key={movieDetailIndex[i][index]}
-                                        key={movieDetailIndex[i][index]}
-                                    >
-                                        <button
-                                            className={`
+                                        key={i}
+                                        className="">
+                                        <p>{date}</p>
+                                        <div className=" flex flex-row gap-2">
+                                            {movieDetailShowCase[i].map((data: any, index: number) => (
+                                                <div
+                                                    // key={movieDetailIndex[i][index]}
+                                                    key={movieDetailIndex[i][index]}
+                                                >
+                                                    <button
+                                                        className={`
                                                         flex justify-center 
                                                         items-center border rounded-md w-[120px] h-[36px]
                                                          hover:bg-slate-500 duration-100 hover:text-white hover:cursor-pointer 
                                                          ${isThatButton(index.toString() + i.toString())}
                                                         `}
-                                            onClick={() => { setShowTime(index.toString() + i.toString()), setMovieIndex(movieDetailIndex[i][index]), setShowDate(date) }}
-                                        >
-                                            <p className="">{data}</p>
-                                        </button>
-                                    </div>
+                                                        onClick={() => { setShowTime(index.toString() + i.toString()), setMovieIndex(movieDetailIndex[i][index]), setShowDate(date) }}
+                                                    >
+                                                        <p className="">{data}</p>
+                                                    </button>
+                                                </div>
 
+                                            ))}
+                                        </div>
+                                    </div>
                                 ))}
                             </div>
                         </div>
-                    ))}
-                </div>
-            </div>
 
 
-            <div className="w-full p-4 mr-5">
-                <div className="font-serif font-semibold text-2xl">SEATING</div>
-                <div className="py-10">
-                    <p className="font-semibold text-2xl">{movieDetailSeparate[movieIndex].title}</p>
-                    <SeatDetail />
-                    <p className="flex justify-center font-semibold text-3xl">SCREEN</p>
-                    <div className="relative mt-3 mb-20">
-                        <div className="absolute bottom-0 left-0 right-0 border-b-2 border-slate-700"></div>
-                    </div>
-                    {/* <div className="flex flex-wrap px-30 "> */}
-                    <div className="grid grid-cols-12  w-3/5 mx-auto">
-                        {/* {movieDetailSeparate[movieIndex].seat_available.map((data: string, index: number) => ( */}
-                        {seat.map((data: string, index: number) => (
-                            <div key={index} >
-                                {movieDetailSeparate[movieIndex].seat_available[index][`${data.toString()}`] ? (
-                                    <button
-                                        className={`${isSelected[index] ? "selected" : "select"}`}
-                                        onClick={() => { handleSelectSeat(data, index) }}
-                                    >
-                                    </button>
-                                ) : (
-                                    <button className="bg-[#898989] hover:cursor-pointer border-2 border-[#898989] border-opacity-90 w-6 h-6 m-1"></button>
-                                )}
-                            </div>
-                        ))
-                        }
-                    </div>
-                    <div className="text-2xl font-bold">ที่นั่ง</div>
-                    <div className=" w-full bg-red h-[40px]">
-                        <div className="flex flex-row w-full">
-                            {reserveSeat.map((v, index) => (
-                                <div
-                                    key={index}
-                                    className="mx-1"
-                                >
-                                    {index == reserveSeat.length - 1 ? (
-                                        <div>
-                                            <p className="">{v}</p>
-                                        </div>
-                                    ) : (
-                                        <div>
-                                            <p className="">{v},</p>
-                                        </div>
-                                    )}
+                        <div className="w-full p-4 mr-5">
+                            <div className="font-serif font-semibold text-2xl">SEATING</div>
+                            <div className="py-10">
+                                <p className="font-semibold text-2xl">{movieDetailSeparate[movieIndex].title}</p>
+                                <SeatDetail />
+                                <p className="flex justify-center font-semibold text-3xl">SCREEN</p>
+                                <div className="relative mt-3 mb-20">
+                                    <div className="absolute bottom-0 left-0 right-0 border-b-2 border-slate-700"></div>
                                 </div>
-                            ))}
-                        </div>
-                    </div>
-                    <div className="w-full bg-slate-600 flex justify-center items-center">
-                        <button onClick={() => {
-                        }}>
-                            {reserveSeat.length === 0 ?
-                                (
-                                    <p className="p-4 text-5xl text-white">ชำระเงิน </p>
-                                ) : (
-                                    <CheckOut
-                                        totalPrice={reserveSeat.length * 150}
-                                        session={session}
-                                        movie_name={movieList?.title}
-                                        movie_id={movieDetailSeparate[movieIndex].movie_id}
-                                        reserveSeat={reserveSeat}
-                                        date={showDate}
-                                    />
-                                )}
-                        </button>
-                    </div>
-                </div>
-            </div >
+                                {/* <div className="flex flex-wrap px-30 "> */}
+                                <div className="grid grid-cols-12  w-3/5 mx-auto">
+                                    {/* {movieDetailSeparate[movieIndex].seat_available.map((data: string, index: number) => ( */}
+                                    {seat.map((data: string, index: number) => (
+                                        <div key={index} >
+                                            {movieDetailSeparate[movieIndex].seat_available[index][`${data.toString()}`] ? (
+                                                <button
+                                                    className={`${isSelected[index] ? "selected" : "select"}`}
+                                                    onClick={() => { handleSelectSeat(data, index) }}
+                                                >
+                                                </button>
+                                            ) : (
+                                                <button className="bg-[#898989] hover:cursor-pointer border-2 border-[#898989] border-opacity-90 w-6 h-6 m-1"></button>
+                                            )}
+                                        </div>
+                                    ))
+                                    }
+                                </div>
+                                <div className="text-2xl font-bold">ที่นั่ง</div>
+                                <div className=" w-full bg-red h-[40px]">
+                                    <div className="flex flex-row w-full">
+                                        {reserveSeat.map((v, index) => (
+                                            <div
+                                                key={index}
+                                                className="mx-1"
+                                            >
+                                                {index == reserveSeat.length - 1 ? (
+                                                    <div>
+                                                        <p className="">{v}</p>
+                                                    </div>
+                                                ) : (
+                                                    <div>
+                                                        <p className="">{v},</p>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className="w-full bg-slate-600 flex justify-center items-center">
+                                    <button onClick={() => {
+                                    }}>
+                                        {reserveSeat.length === 0 ?
+                                            (
+                                                <p className="p-4 text-5xl text-white">ชำระเงิน </p>
+                                            ) : (
+                                                <CheckOut
+                                                    totalPrice={reserveSeat.length * 150}
+                                                    session={session}
+                                                    movie_name={movieList?.title}
+                                                    movie_id={movieDetailSeparate[movieIndex].movie_id}
+                                                    reserveSeat={reserveSeat}
+                                                    date={showDate}
+                                                />
+                                            )}
+                                    </button>
+                                </div>
+                            </div>
+                        </div >
+                    </>)
+                        :
+                        (
+                            <>
+                                <div className="w-full p-4">
+                                    <div className="font-serif font-semibold text-2xl">SHOWTIMES</div>
+                                </div>
+
+
+                                <div className="w-full p-4 mr-5">
+                                    <div className="font-serif font-semibold text-2xl">SEATING</div>
+                                </div >
+                            </>
+                        )
+                }
+            </>
+
+
         </div >
 
 
--- utils/time.ts
+++ utils/time.ts
@@ -4,9 +4,9 @@ export const GetLocalTime = () => {
     const year = now.getFullYear();
     const month = String(now.getMonth() + 1).padStart(2, '0'); // Adjust month to 1-based index
     const day = String(now.getDate()).padStart(2, '0');
-    const hour = String(now.getHours()).padStart(2, '0');
-    const minute = String(now.getMinutes()).padStart(2, '0');
-    const second = String(now.getSeconds()).padStart(2, '0');
+    // const hour = String(now.getHours()).padStart(2, '0');
+    // const minute = String(now.getMinutes()).padStart(2, '0');
+    // const second = String(now.getSeconds()).padStart(2, '0');
 
-    return `${year}-${month}-${day}:${hour}:${minute}:${second}`;
+    return `${year}-${month}-${day}`;
 }
